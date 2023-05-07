f22fdbcd5b77d171b67b0447d042eed4735735c4
Mary <mashenka.koryagina@inbox.ru>
2023-05-07 14:28:12 +0300

    the solution of task has been made

---
+++ Homework_seminarC#/Program.cs
@@ -0,0 +1,192 @@
+﻿//Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт,
+// какое число большее, а какое меньшее.
+/*
+Console.Write("Input 1st number: ");
+int number_1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Input 2nd number: ");
+int number_2 = Convert.ToInt32(Console.ReadLine());
+
+if (number_1 > number_2)
+{
+    int max = number_1;
+     Console.WriteLine($"Your first number {number_1} is the maximum");
+    int min = number_2;
+    Console.WriteLine($"Your second number {number_2} is the minimum");
+}
+else
+{
+    int max = number_2;
+     Console.WriteLine($"Your second number {number_2} is the maximum");
+    int min = number_1;
+    Console.WriteLine($"Your first number {number_1} is the minimum");
+}
+*/
+
+//Задача 4: Напишите программу, которая принимает на вход
+// три числа и выдаёт максимальное из этих чисел.
+
+
+/*
+Console.Write("Input 1st number: ");
+int number_1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Input 2nd number: ");
+int number_2 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Input 3rd number: ");
+int number_3 = Convert.ToInt32(Console.ReadLine());
+
+int number_max = number_1;
+
+if (number_1 > number_2)
+{
+     number_max = number_1;
+     Console.WriteLine($"Your first number {number_1} is the maximum");
+}
+if(number_2 > number_max)
+{
+     number_max = number_2;
+     Console.WriteLine($"Your second number {number_2} is the maximum");
+}
+if (number_3 > number_max)
+{
+     number_max = number_3;
+     Console.WriteLine($"Your third number {number_3} is the maximum");
+}
+*/
+
+//Задача 6: Напишите программу, которая на вход принимает число и выдаёт,
+//является ли число чётным (делится ли оно на два без остатка).
+/*
+Console.Write("Input your number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if ((number % 2) == 0)
+{
+    Console.WriteLine($"Your number {number} is the even number");
+}
+
+else
+{
+    Console.WriteLine($"Your number {number} is the odd number");
+}
+*/
+
+//Задача 8: Напишите программу, которая на вход принимает число (N),
+// а на выходе показывает все чётные числа от 1 до N.
+/*
+Console.Write("Input your number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int current_i = 1;
+
+while (current_i <= number)
+{
+    if (current_i % 2 == 0)
+    {
+        Console.Write(current_i + " ");
+        current_i ++;
+        }
+
+    else
+    {
+        current_i ++;
+    }
+}
+*/
+
+//Задача 10: Напишите программу, которая принимает на вход 
+//трёхзначное число и на выходе показывает вторую цифру этого числа
+
+
+/*
+void Numb( int user_number)
+{
+    if (user_number > 99 && user_number < 1000 ||user_number > -1000 && user_number < -99  )
+    {
+        int digit1 = user_number % 10;// 456 % 10 -> 6
+        int digit2 = user_number / 10;  // 456 / 10 -> 45
+        int digit3 = digit2 % 10; //45 % 10 -> 5
+        Console.WriteLine($"Your number is {digit3}");
+    }
+
+    else
+    {
+        Console.WriteLine($"Your number is not 3-digit");
+    }
+
+}
+
+Console.WriteLine("Input number: ");
+int user_number = Convert.ToInt32(Console.ReadLine());
+
+Numb(user_number);
+
+*/
+//Задача 13: Напишите программу, которая выводит третью
+// цифру заданного числа или сообщает, что третьей цифры нет.
+/*
+void Numb(int user_number)
+{
+    if (user_number > 99  ||  user_number < -99)
+    {   
+        while(user_number > 999 || user_number < -999 )
+        {
+            user_number /=  10;
+        }
+            if (user_number > 99)
+            {
+                int digit1 = user_number % 10;// 456 % 10 -> 6
+                Console.WriteLine($"The third digit of this number is {digit1}.");
+            }
+
+            else 
+            {
+                int positive_number = user_number * -1;
+                int digit2 = positive_number % 10;// 456 % 10 -> 6
+                Console.WriteLine($"The third digit of this number is {digit2}.");
+            }
+        
+    }
+
+    else
+    {
+        Console.WriteLine($"The given number does not have a third digit.");
+    }
+    
+}
+Console.WriteLine("Input number: ");
+int user_number = Convert.ToInt32(Console.ReadLine());
+
+ Numb(user_number);
+
+*/
+
+//Задача 15: Напишите программу, которая принимает на вход цифру,
+// обозначающую день недели, и проверяет, является ли этот день выходным.
+/*
+void Week(int day_week)
+{
+if (day_week > 0 && day_week < 8)
+{
+    if (day_week < 6)
+    {
+        Console.WriteLine($"It's a working day, we still need to work.");
+    }
+    else
+    {
+        Console.WriteLine($"Congratulations! Today is a day off, have a rest!");
+    }
+}
+else 
+{
+    Console.WriteLine($"Sorry. But this number does not correspond to the number of the week. Try again..");
+}
+}
+Console.WriteLine("Input number: ");
+int day_week = Convert.ToInt32(Console.ReadLine());
+
+Week(day_week);
+
+*/
---
+++ Seminar_C#/Program.cs
@@ -0,0 +1,407 @@
+﻿/*
+//Console.WriteLine("Good morning");
+Console.WriteLine("Input your number: ");
+int number = Convert.ToInt32(Console.ReadLine ());
+//Console.WriteLine(number);
+*/
+
+//Задача 1
+// Напишите программу, которая на вход принимает число и выдает его квадрат
+/*
+Console.Write("Input your number: ");
+int number_1 = Convert.ToInt32(Console.ReadLine());
+
+int result = number_1 * number_1;
+
+//Console.WriteLine (number_1 * number_1);
+Console.WriteLine ("Your number is " + number_1 + ". Square of this number is " + result);
+Console.WriteLine ($"Your number is {number_1}. Square of this number is {result}");
+*/
+//Задача 3.
+//Напишите программу, которая на вход принимает два числа и проверяет,
+//является ли первое число квадратом второго.
+
+// a = 5, b = 25 -> no
+// a = 25, b = 5 -> yes
+/* миниалгоритм
+1. запросить два значения ( 11+12 строки) +
+2. возвести 2е число в квадрат
+3. проверить на равенство первому числу
+
+
+Console.Write("Input 1st number: ");
+int number_1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input 2nd number: ");
+int number_2 = Convert.ToInt32(Console.ReadLine());
+
+if (number_2 * number_2 == number_1)
+{
+    Console.WriteLine($"Your first number is square of the second one");
+}
+else 
+{
+   Console.WriteLine($"Your first number is not square of the second one"); 
+}
+
+
+// Задача 5.
+// Напишите программу, которая на вход принимает одно число(N),
+// а  на выходе показывает все  целые числа в промежутке от - N до N.
+
+Console.Write("Input number: ");
+int number_N = Convert.ToInt32(Console.ReadLine());
+
+int current_i = number_N * (-1);
+
+while (current_i <= number_N)
+{
+    Console.Write(current_i + " ");
+    current_i ++ ;// current_i += 1; current_i = current_i + 1
+}
+*/
+
+// Задача 7.
+//Напишите программу, которая принимает на вход трёхзначное число и
+// на выходе показывает последнюю цифру этого числа.
+
+// 1. Ввод числа
+// 2. Проверить его на 3х значность (N > 99 + N < 1000) +  отрицательные значения
+// 3. Найти последнюю цифру (%10) % - отстаток от деления
+
+//456 % 10 -> 6
+// 456 % 100 ->56
+//456 / 10 -> 45     / - деление нацело
+//456 % 100 -> 4
+/*
+Console.Write("Input number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+//int positive_number = number ( если number > 0) OR number * (-1)
+if (number > 99 && number < 1000) // ||number > -1000 && number < -99  )   // && (или &) - логич оператор И      || - логич оператор ИЛИ
+{
+    //int dev = number % 10;
+    //Console.WriteLine( $"Last digit {number} is {dev}");
+    Console.WriteLine( "Last num is: "  + number%10) ;
+}
+else
+{
+    Console.WriteLine(" Your number is not 3-digit");
+}
+*/
+
+//Напишите программу, которая получает на вход
+// двузначное число и показывает наибольшую цифру числа.
+
+// тип метода Имя метода (аргументы)
+
+/*
+int Digits(int number)
+{
+// 1. выделить цифры числа
+int ed = number % 10;
+int dec = number / 10;
+//2. сравнить цифры
+if (ed>dec)
+{
+    return ed;
+}
+else
+{
+    return dec;
+}
+//3. сообщить результат
+
+}
+
+*/
+/*
+void Digit1(int number)                //метод сравнения
+{
+    int ed = number % 10;
+    int dec = number / 10;
+    if (ed > dec)
+    {
+        Console.WriteLine($"The biggest digit is {ed}");
+    }
+    else if(ed == dec)
+    {
+        Console.WriteLine($"The digits are equal");
+    }
+    else
+    {
+        Console.WriteLine($"The biggest digit is {dec}");
+    }
+}*/
+/*
+Console.WriteLine("Input number: ");
+int user_number = Convert.ToInt32(Console.ReadLine());
+
+if (user_number > 9 && user_number < 100)
+{
+    Digit1(user_number);    //вызов метода
+   // int res = Digits(user_number);
+   // Console.WriteLine($"The biggest digit is {res}");
+}
+ else
+{
+    Console.WriteLine("Impossible value! ");
+}
+*/
+
+//Напишите программу, которая выводит случайное число из отрезка [10, 99] и показывает наибольшую цифру числа.
+//78 -> 8
+//12-> 2
+//85 -> 8
+
+/* 1.невозвратный метод без аргументов
+2. генератор*/
+/*
+void MaxDigit()         // создали метод невозвратный и безаргументный
+{
+
+int minVal = 10;
+    int maxVal = 99;
+    int randomNum = new Random().Next(minVal, maxVal + 1);         // выполнили генерацию из полуинтервала [10, 100]
+    Console.WriteLine($"Your number is {randomNum}"); // вывели пользователю сгенерированные значения
+    Digit1(randomNum);     // вызвали метод сравнения разрядов
+
+}
+
+MaxDigit();*/
+
+//Напишите программу, которая принимает
+// на вход число и проверяет, кратно ли оно одновременно a и b.
+/*
+void Cratnost (int a, int b, int num) // метод проверки на кратность
+{
+    if ( num % a == 0 && num % b == 0 )
+    {
+        Console.WriteLine($"Your number {num} is multiple of {a} and {b} ");
+    }
+    else
+    {
+        Console.WriteLine($"Your number {num} is not multiple of {a} and {b} ");
+    }
+}
+*/
+//запрос данных у пользователя
+/*
+Console.WriteLine("Input your number: ");       //запрос самого числа
+int new_user_num = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Input 1st  divider: ");   // запрос 1делителя
+int div1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Input 2nd  divider: ");      // запрос 2 делителя
+int div2 = Convert.ToInt32(Console.ReadLine());
+
+Cratnost (div1, div2, new_user_num); // вызов метода
+*/
+
+
+//Напишите программу, которая выводит случайное трёхзначное число и удаляет вторую цифру этого числа.
+//456 -> 46, 789 -> 79
+/*
+int DeletDec()
+{
+    int randomNumber = new Random().Next(100, 1000);
+    Console.WriteLine($"Your number is {randomNumber}");
+
+    int digit1 = randomNumber/ 100; // 456 / 100 -> 4
+    int digit2 = randomNumber % 10; // 456 % 10 -> 6
+    int new_num = digit1 * 10 + digit2;
+    return new_num;
+}
+
+Console.WriteLine($"Your number is {DeletDec()}");
+*/
+
+// //Напишите программу, которая принимает на вход координаты точки (X и Y),
+//причём X ≠ 0 и Y ≠ 0 и выдаёт номер четверти плоскости, в которой находится эта точка.
+
+/*          |y
+            |
+            |
+  II        |           I
+            |
+            |
+___________________________x 
+            |
+            |
+ III        |         IV
+            |
+            |
+            |       
+
+ I x>0, y>0
+ II x<0, y>0
+ III x<0, y<0
+ IV x>0, y<0   
+ */
+/*
+void Koords(int x, int y)
+{
+    if (x > 0 && y > 0)
+        Console.WriteLine("This point on 1st quater");
+    else if (x < 0 && y > 0)
+        Console.WriteLine("This point on 2nd quater");
+    else if (x < 0 && y < 0)
+        Console.WriteLine("This point on 3rd quater");
+    else 
+        Console.WriteLine("This point on 4st quater");
+}
+
+Console.WriteLine("Enter your X coordinste: ");
+int xCoord = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Enter your Y coordinste: ");
+int yCoord = Convert.ToInt32(Console.ReadLine());
+
+if (xCoord == 0 || yCoord == 0 )
+    Console.WriteLine("Its impossible value for this task!");
+else 
+    Koords(xCoord, yCoord ); 
+
+*/
+/*
+int Koords(int x, int y)
+{
+    int number_quater;
+    if (x > 0 && y > 0)
+        number_quater = 1;
+    else if (x < 0 && y > 0)
+       number_quater = 2;
+    else if (x < 0 && y < 0)
+        number_quater = 3;
+    else 
+        number_quater = 4;
+
+return number_quater;
+}
+
+Console.WriteLine("Enter your X coordinste: ");
+int xCoord = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Enter your Y coordinste: ");
+int yCoord = Convert.ToInt32(Console.ReadLine());
+
+if (xCoord == 0 || yCoord == 0 )
+{
+Console.WriteLine("Its impossible value for this task!");
+    return;
+}
+else     Koords(xCoord, yCoord ); 
+
+int quater = Koords(xCoord, yCoord );
+Console.WriteLine(quater);
+*/
+
+
+//Напишите программу, которая по заданному номеру четверти,
+//показывает диапазон возможных координат точек в этой четверти (x и y).
+/*
+void PossibleValue(int num)
+{
+    if (num == 1)
+        Console.WriteLine("1st quater x > 0, y > 0");
+    else if (num == 2)
+        Console.WriteLine("2nd quater x < 0, y > 0");
+
+    else if (num == 3)
+        Console.WriteLine("3rd quater x < 0, y < 0");
+    else 
+        Console.WriteLine("4st quater x > 0, y < 0 ");
+}
+Console.WriteLine("Input number of quater: ");
+int numQuater = Convert.ToInt32(Console.ReadLine());
+
+if (numQuater >= 1 && numQuater <= 4)
+        PossibleValue(numQuater);
+else Console.WriteLine("Its not number of quater!");
+*/
+
+//Напишите программу, которая принимает на вход координаты двух точек и
+//находит расстояние между ними в 2D пространстве.
+//A(x1, y1); B(x2, y2)
+//Math.Sqrt() - извлечение квадратного корня
+/*
+  _________________________________________
+\/(x1 - x2)(x1 - x2) + (y1 - y2)(y1 - y2)
+*/
+/*
+double LongLine(int x1, int x2, int y1, int y2)
+{
+    double lengthAB = Math.Sqrt((x1 - x2)*(x1 - x2) + (y1 - y2)*(y1 - y2));
+    //double lengthAB1 = Math.Round (lengthAB, 2);// метод округления
+    return lengthAB;
+}
+
+Console.WriteLine("Input X coordinate of A : ");
+int xCoordA = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Input Y coordinate of A : ");
+int yCoordA = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Input X coordinate of B : ");
+int xCoordB = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Input Y coordinate of B : ");
+int yCoordB = Convert.ToInt32(Console.ReadLine());
+
+double dist = LongLine (xCoordA, xCoordB, yCoordA, yCoordB );
+
+Console.WriteLine($"Distance AB is {dist:f3} ");// метод округления номер 2
+
+*/
+
+//Напишите программу, которая принимает на вход число (N) и
+//выдаёт таблицу квадратов чисел от 1 до N.
+//5 -> 1, 4, 9, 16, 25.
+//2 -> 1,4
+/*
+void SquareFinder()
+{
+    Console.WriteLine("Input number: ");
+    int numberN = Convert.ToInt32(Console.ReadLine());
+
+    Console.WriteLine("Input degree: ");
+    int degree = Convert.ToInt32(Console.ReadLine());
+
+    if(numberN > 0)
+    {
+        int current = 1;
+        while (current <= numberN)
+        {
+            double res = Math.Pow(current,degree);
+            Console.Write($"{res:f0}, ");
+            current ++;
+        }
+        Console.WriteLine("\b\b.");
+    }
+    else 
+   {
+    int current = 1;
+     while (current >= numberN)
+        {
+            int res = current * current;
+            Console.Write($"{res}, ");
+            current --;
+        }
+        Console.WriteLine("\b\b.");
+   }
+}
+
+SquareFinder();
+
+*/
+
+
+
+
+
+
+
+
+
+
+
