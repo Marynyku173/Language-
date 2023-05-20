d81a968f22706b059cdfc1ed64176931aceb17e0
Mary <mashenka.koryagina@inbox.ru>
2023-05-20 08:29:59 +0300

    solving solutions at a lecture

---
+++ Example_Methods/Program.cs
@@ -0,0 +1,204 @@
+﻿// Вид 1
+/*
+void Method1()
+{
+    Console.WriteLine("Автор ...");
+}
+Method1();
+*/
+
+//Вид 2
+/*
+void Method2(string msg)
+{
+    Console.WriteLine(msg);
+}
+Method2(msg: "Текст сообщения");
+*/
+/*
+void Method21(string msg, int count)
+{
+    int i = 0;
+    while(i < count)
+    {
+        Console.WriteLine(msg);
+        i++;
+    }
+   
+}
+//Method21(msg:"Текст",count: 4);
+Method21(count: 4,msg:"Текст");
+*/
+/*Вид 3
+int Method3()
+    {
+        return DateTime.Now.Year;
+    }
+
+int year = Method3();
+Console.WriteLine(year);
+*/
+//Вид 4
+/*
+string Method4(int count, string text)
+{
+    int i = 0;
+    string result = string.Empty;
+
+    while(i < count)
+    {
+        result = result + text;
+        i++;
+    }
+    return result;
+}
+string res =  Method4(10, "я");
+Console.WriteLine(res);
+*/
+/*
+string Method4(int count, string text)
+{
+    string result = string.Empty;
+    for( int i = 0; i < count;  i++)
+    {
+        result = result + text;
+       
+    }
+    return result;
+}
+string res =  Method4(10, "я");
+Console.WriteLine(res);
+*/
+
+//Цикл в цикле
+//Таблица умножения
+/*
+for (int i = 2; i <= 10; i++)
+{
+    for (int j = 2; j <= 10; j++)
+    {
+        Console.WriteLine($"{i} x {j} = {i * j}");
+    }
+    Console.WriteLine();
+}
+*/
+
+//+++++РАбота с текстом
+//=====Работа с текстом
+// Дан текст. В тексте нужно все пробелы заменить чёрточками,
+// маленькие буквы “к” заменить большими “К”,
+// а большие “С” маленькими “с”.
+// Ясна ли задача?
+/*
+string text = "— Я думаю, — сказал князь, улыбаясь, — что,"
+            + "ежели бы вас послали вместо нашего милого Винценгероде,"
+            + "вы бы взяли приступом согласие прусского короля."
+            + "Вы так красноречивы. Вы дадите мне чаю?";
+*/
+// string s = "qwerty"
+//             012345
+// s[3] = r 
+// разобраться вот с этим
+/*
+string Replace(string text, char oldValue, char newValue )
+{
+    string result = string.Empty;
+
+    int length = text.Length;
+    for (int i = 0; i < length; i++)
+    {
+        if(text[i] == oldValue) result = result + $"{newValue}";
+        else result = result + $"{text[i]}";
+    }
+
+     return result;
+}
+
+string newText = Replace(text,  ' ', '|' );
+Console.WriteLine(newText);
+Console.WriteLine();
+newText = Replace(newText,  'к', 'К' );
+Console.WriteLine(newText);
+Console.WriteLine();
+newText = Replace(newText,  'С', 'с' );
+Console.WriteLine(newText);
+*/
+
+//Упорядочить массивы от меньшего к большему
+//Было: 6 8 3 2 1 4 5 7
+//Стало: 1 2 3 4 5 6 7 8
+/*
+int[] arr = {1, 5, 4, 3, 2, 6, 7, 1, 1};
+
+void PrintArray(int[] array)
+{
+    int count = array.Length;
+    for(int i = 0; i < count; i++)
+    {
+        Console.WriteLine($"{array[i]}");
+    }
+    Console.WriteLine();
+}
+
+void SelectionSort(int[] array)
+{
+
+     for (int i = 0; i < array.Length - 1; i++)
+     {
+        int minPosition = i;
+
+        for(int j = i + 1; j < array.Length; j++)
+        {
+            if(array[j] < array[minPosition]) minPosition = j;
+        }
+        
+        int temporary = array[i];
+        array[i] = array[minPosition];
+        array[minPosition] = temporary;
+     }
+}
+
+PrintArray(arr);
+SelectionSort(arr);
+
+PrintArray(arr);
+*/
+//Упорядочить массивы от большего к меньшему
+//Было: 6 8 3 2 1 4 5 7
+//Стало: 8 7 6 5 4 3 2 1
+/*
+int[] arr = {1, 5, 4, 3, 2, 6, 7, 1, 1};
+
+void PrintArray(int[] array)
+{
+    int count = array.Length;
+    for(int i = 0; i < count; i++)
+    {
+        Console.WriteLine($"{array[i]}");
+    }
+    Console.WriteLine();
+}
+
+void SelectionSort(int[] array)
+{
+
+     for (int i = 0; i < array.Length - 1; i++)
+     {
+        int maxPosition = i;
+
+        for(int j = i + 1; j < array.Length; j++)
+        {
+            if(array[j] > array[maxPosition]) maxPosition = j;
+        }
+        
+        int temporary = array[i];
+        array[i] = array[maxPosition];
+        array[maxPosition] = temporary;
+     }
+}
+
+PrintArray(arr);
+SelectionSort(arr);
+
+PrintArray(arr);
+*/
