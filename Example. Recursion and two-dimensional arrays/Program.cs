12a4417281a2690f8277774cd8c2efcd8faae44f
Mary <mashenka.koryagina@inbox.ru>
2023-06-24 16:59:09 +0300

    added lection

--- Example. Recursion and two-dimensional arrays/Program.cs
+++ Example. Recursion and two-dimensional arrays/Program.cs
@@ -124,7 +124,7 @@ for(int i = 1; i < 40; i++)
 а(2) = 1
 а(n) = a(n -1 ) + a(n - 2)
 */
-
+/*
 double Fibonacci(int n)
 {
     if (n == 1 || n == 2) return 1;
@@ -135,3 +135,167 @@ double Fibonacci(int n)
  {
     Console.WriteLine($"f({i}) = {Fibonacci(i)}");
  }
+*/
+
+
+
+// Собрать строку с числами от a до b, a <= b
+/*
+string NumbersFor(int a, int b)// метод без рекурсии , с помощью циклов
+{
+    string result = string.Empty;
+    for (int i = a; i <= b; i++)
+    {
+        result += $"{i} ";
+    }
+    return result;
+}
+
+string NumbersRec(int a, int b)// Метод с рекурсией
+{
+    if (a <= b) return $"{a} " + NumbersRec(a + 1, b);
+    else return string.Empty;
+}
+
+Console.WriteLine(NumbersFor(1, 10));
+Console.WriteLine();
+Console.WriteLine(NumbersRec(1, 10));
+
+*/
+/*
+
+string NumbersFor(int a, int b)// метод без рекурсии , с помощью циклов
+{
+    string result = string.Empty;
+    for (int i = a; i >= b; i--)
+    {
+        result += $"{i} ";
+    }
+    return result;
+}
+
+string NumbersRec(int a, int b)// Метод с рекурсией
+{
+    if (a <= b) return NumbersRec(a + 1, b) + $"{a} ";
+    else return string.Empty;
+}
+
+
+Console.WriteLine(NumbersFor(10, 1));
+Console.WriteLine();
+Console.WriteLine(NumbersRec(1, 10));
+*/
+
+
+
+//Сумма чисел от 1 до n
+
+/*
+int SumFor(int n)
+{
+    int result = 0;
+    for (int i = 1; i <= n; i++) result += i;
+    return result;
+}
+
+
+int SumRec(int n)
+{
+    if (n == 0) return 0;
+    else return n + SumRec(n - 1);
+
+}
+
+Console.WriteLine(SumFor(10));
+Console.WriteLine();
+Console.WriteLine(SumRec(10));
+*/
+
+//Факториал числа
+/*
+int FactorialFor(int n)
+{
+    int result = 1;
+    for (int i = 1; i <= n; i++) result *= i;
+    return result;
+}
+
+int FactorialRec(int n)
+{
+    if (n == 1) return 1;
+    else return n * FactorialRec(n - 1);
+}
+
+
+Console.WriteLine(FactorialFor(10));
+Console.WriteLine();
+Console.WriteLine(FactorialRec(10));
+*/
+// Возвести a в степень n
+/*
+int PowerFor(int a, int n)
+{
+    int result = 1;
+    for (int i = 1; i <= n; i++) result *= a;
+    return result;
+}
+
+int PowerRec(int a, int n)
+{
+    return n == 0 ? 1 : PowerRec(a, n - 1) * a;
+   // if (n == 0) return 1;
+   // else return PowerRec(a, n - 1) * a;
+
+}
+
+Console.WriteLine(PowerFor(2, 10));
+Console.WriteLine();
+Console.WriteLine(PowerRec(2, 10));
+*/
+/*
+int PowerRecMath(int a, int n)
+{
+
+    if (n == 0) return 1;
+    else if (n % 2 == 0) return PowerRecMath(a * a, n / 2);
+    else return PowerRecMath(a, n - 1) * a;
+
+}
+
+Console.WriteLine(PowerRecMath(2, 9));
+*/
+
+//Перебор слов
+//Следующий пример немного сложнее. Пусть у нас будет некоторый алфавит, состоящий из
+//четырёх букв, и нас просят показать все возможные слова, состоящие из T букв. Т может
+//равняться 1, 2, 3 и так далее.
+/*
+string path = @"C:/Users/12345/Desktop/Language C#/Examples/Example002_HelloUser";
+DirectoryInfo di = new DirectoryInfo(path);
+System.Console.WriteLine(di.CreationTime);
+FileInfo[] fi = di.GetFiles();
+
+for  (int i = 0; i < fi.Length; i++)
+{
+    System.Console.WriteLine(fi[i].Name);
+}
+*/
+void CatalogInfo(string path, string indent = "")
+{
+    DirectoryInfo catalog = new DirectoryInfo(path);
+    DirectoryInfo[] catalogs = catalog.GetDirectories();
+    for (int i = 0; i < catalogs.Length; i++)
+    {
+        Console.WriteLine($"{indent}{catalogs[i].Name}");
+        CatalogInfo(catalogs[i].FullName, indent + " ");
+    }
+    FileInfo[] files = catalog.GetFiles();
+
+    for (int i = 0; i < files.Length; i++)
+    {
+        Console.WriteLine($"{indent}{files[i].Name}");
+
+    }
+}
+string path = @"C:/Users/12345/Desktop/Language C#/Examples";
+CatalogInfo(path);
