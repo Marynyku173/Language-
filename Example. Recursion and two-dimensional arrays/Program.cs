3aaca889df5942140f5389ba3cf2f4f3859bae7e
Mary <mashenka.koryagina@inbox.ru>
2023-06-25 09:13:24 +0300

    added lection

--- Example. Recursion and two-dimensional arrays/Program.cs
+++ Example. Recursion and two-dimensional arrays/Program.cs
@@ -280,6 +280,7 @@ for  (int i = 0; i < fi.Length; i++)
     System.Console.WriteLine(fi[i].Name);
 }
 */
+/*
 void CatalogInfo(string path, string indent = "")
 {
     DirectoryInfo catalog = new DirectoryInfo(path);
@@ -299,3 +300,138 @@ void CatalogInfo(string path, string indent = "")
 }
 string path = @"C:/Users/12345/Desktop/Language C#/Examples";
 CatalogInfo(path);
+*/
+
+//Игра в пирамидки
+/*
+void Towers(string with = "1", string on = "3", string some = "2", int count = 5)
+{
+    if (count > 1) Towers(with, some, on, count - 1);
+    Console.WriteLine($"{with} >> {on}");
+    if(count > 1 ) Towers (some, on, with, count - 1);
+}
+Towers();
+*/
+
+
+//Написание задачи(4-2)*(1+3)/10
+/*
+string emp = string.Empty;
+string[] tree = { emp, "/", "*", "10", "-", "=", emp, emp, "4", "2", "1", "3" };
+//
+void InOrderTraversal(int pos = 1)
+{
+    if (pos < tree.Length)
+    {
+        int left = 2 * pos;
+        int right = 2 * pos + 1;
+        if (left < tree.Length && !String.IsNullOrEmpty(tree[left])) InOrderTraversal(left);
+        Console.Write(tree[pos]);
+        if (right < tree.Length && !String.IsNullOrEmpty(tree[right])) InOrderTraversal(right);
+    }
+}
+InOrderTraversal();
+*/
+
+/*
+decimal fRec = 0;
+decimal fIte = 0;
+
+decimal FibonacciRecursion(int n)
+{
+    fRec++;
+    return n == 0 || n == 1 ? 1 : FibonacciRecursion(n - 1) + FibonacciRecursion(n - 2);
+}
+
+
+decimal FibonacciIteration(int n)
+{
+    fIte++;
+    decimal result = 1;
+    decimal f0 = 1;
+    decimal f1 = 1;
+    for (int i = 2; i <= n; i++)
+    {
+        result = f0 + f1;
+
+        f0 = f1;
+        f1 = result;
+        fIte++;
+    }
+    return result;
+}
+Console.ReadLine();
+
+DateTime dt = DateTime.Now;
+
+for (int n = 10; n < 40; n++)
+{
+    Console.WriteLine($"FibonacciIteration({n}) = {FibonacciIteration(n)} fIte = {fIte.ToString("### ### ###"), -15 }");
+    fIte = 0;
+}
+
+System.Console.WriteLine((DateTime.Now - dt).TotalMilliseconds);
+Console.WriteLine();
+Console.ReadLine();
+
+ dt = DateTime.Now;
+for (int n = 10; n < 40; n++)
+{
+    Console.WriteLine($"FibonacciRecursion({n}) = {FibonacciRecursion(n)} fRec = {fRec.ToString("### ### ###"), -15}");
+    fRec = 0;
+}
+System.Console.WriteLine((DateTime.Now - dt).TotalMilliseconds);
+Console.ReadLine();
+
+*/
+/*
+int i = 0;
+void Rec()
+{
+    System.Console.WriteLine(i++);
+    Rec();
+}
+Rec();
+*/
+
+
+
+int[,] pic = new int[1000, 1000];
+for(int i =0; i < 10; i++)
+{
+    pic [0,i] = 1;
+    pic [i,0] = 1;
+    pic [i,9] = 1;
+    pic [9,i] = 1;
+}
+
+PrintImage(pic);
+FillImage(1, 1);
+Console.WriteLine();
+PrintImage(pic);
+
+void PrintImage(int[,] image)
+{
+    for(int i = 0; i < image.GetLength(0); i++)
+    {
+        for(int j = 0; j < image.GetLength(1); j++)
+        {
+            if(image[i,j] == 0) Console.Write($" ");
+            else Console.Write($"+");
+        }
+    Console.WriteLine();   
+    }
+}
+
+void FillImage(int row, int col)
+{
+    if(pic[row,col] == 0)
+    {
+        pic[row, col] = 1;
+        FillImage(row - 1, col);
+        FillImage(row, col - 1);
+        FillImage(row + 1, col);
+        FillImage(row, col + 1);
+    }
+}
+
